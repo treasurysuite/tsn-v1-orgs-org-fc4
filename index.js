e4a16abefa73e712df076fdadb49228880a9477b
davidstarts <david@convina.com>
2025-03-04 14:55:38 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+module.exports = [
+  {
+    nodule: 'orgs/org/tsn/template',
+    access: [
+      {
+        admin: true,
+      },
+      {
+        user_access: {
+          name: 'org',
+        },
+      },
+    ],
+
+    forms: {
+    },
+    urls: {
+  
+    },
+    models: {},
+  },
+];
